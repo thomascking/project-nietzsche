15a116ecd6e93fce6a8c65475856f92ce17c0af8
Thomas King <king.thomas.c@gmail.com>
2019-11-28 19:47:22 -0500

    make window

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+)
+
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "Nietzsche",
+		Bounds: pixel.R(0, 0, 1024, 768),
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	for !win.Closed() {
+		win.Update()
+	}
+}
 
 func main() {
+	pixelgl.Run(run)
 	fmt.Println("hello, world!")
 }
