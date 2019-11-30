c8c2a1af379b1ee547dbf118e68bd1702f4b5b51
Thomas King <king.thomas.c@gmail.com>
2019-11-29 21:00:22 -0500

    work on maps

--- maps/test.go
+++ maps/test.go
@@ -1,6 +1,7 @@
 package maps
 
 import (
+	"github.com/faiface/pixel"
 	"github.com/thomascking/project-nietzsche/entity"
 	"github.com/thomascking/project-nietzsche/world"
 )
@@ -10,6 +11,7 @@ func NewTestMap(w world.World) {
 	entities := []entity.Entity{
 		newBG("./images/test-level-bg.png"),
 		newFG("./images/test-level-fg.png"),
+		entity.NewPlayer(pixel.V(10, 100)),
 	}
 	for _, e := range entities {
 		w.AddEntity(e)
